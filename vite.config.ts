a56b8b1b9d0e6250aadcb70aa863262fafddb10e
manideep395 <manideep395@gmail.com>
2026-05-11 12:46:24 +0530

    allowedHosts to accept all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     server: {
-      allowedHosts: ["kmr-techies.onrender.com"],
-    },
-  },
-  tanstackStart: {
-    server: {
-      preset: "render-com",
+      allowedHosts: true,
     },
   },
 });
